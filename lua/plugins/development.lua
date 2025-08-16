cdbe1b4dd7f9b22a678313d7e92c82c7ca7b4d45
Jeongho Mok <jngho.m@gmail.com>
2025-08-17 00:05:02 +0900

    update

--- lua/plugins/development.lua
+++ lua/plugins/development.lua
@@ -298,5 +298,20 @@ return {
 
       require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
     end,
-  }
+  },
+
+  {
+    "mistweaverco/kulala.nvim",
+    keys = {
+      { "<leader>Rs", desc = "Send request" },
+      { "<leader>Ra", desc = "Send all requests" },
+      { "<leader>Rb", desc = "Open scratchpad" },
+    },
+    ft = { "http", "rest" },
+    opts = {
+      global_keymaps = false,
+      global_keymaps_prefix = "<leader>R",
+      kulala_keymaps_prefix = "",
+    },
+  },
 }
