18e5bda809c4ca0387bd208b25bcd4778664d675
Jeongho Mok <jngho.m@gmail.com>
2025-08-17 00:49:30 +0900

    textobject

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -118,6 +118,38 @@ return {
     end,
   },
 
+  -- Treesitter Textobjects (movement via syntax-aware objects)
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        textobjects = {
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']f'] = '@function.outer',
+              [']c'] = '@class.outer',
+            },
+            goto_next_end = {
+              [']F'] = '@function.outer',
+              [']C'] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[f'] = '@function.outer',
+              ['[c'] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[F'] = '@function.outer',
+              ['[C'] = '@class.outer',
+            },
+          },
+        },
+      })
+    end,
+  },
+
   -- Oil.nvim: simple file explorer that replaces netrw
   {
     'stevearc/oil.nvim',
