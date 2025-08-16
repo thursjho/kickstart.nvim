2a3d300c16b4a8555751210b6b2bbd8be81f0db6
Jeongho Mok <jngho.m@gmail.com>
2025-08-16 21:57:52 +0900

    fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -36,12 +36,12 @@ return {
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('gd', function() Snacks.picker.lsp_definitions() end, '[G]oto [D]efinition')
+          map('gr', function() Snacks.picker.lsp_references() end, '[G]oto [R]eferences')
+          map('gI', function() Snacks.picker.lsp_implementations() end, '[G]oto [I]mplementation')
+          map('<leader>D', function() Snacks.picker.lsp_type_definitions() end, 'Type [D]efinition')
+          map('<leader>ds', function() Snacks.picker.lsp_symbols() end, '[D]ocument [S]ymbols')
+          map('<leader>ws', function() Snacks.picker.lsp_symbols({ all = true }) end, '[W]orkspace [S]ymbols')
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
