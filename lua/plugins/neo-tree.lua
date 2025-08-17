a9d440fc7c2105eecb3a994e51378788e05caece
Jeongho Mok <jngho.m@gmail.com>
2025-08-17 23:52:17 +0900

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,69 @@ return {
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons",
     },
+    keys = {
+      { '<leader>e', '<cmd>Neotree toggle<cr>', desc = 'Toggle Explorer' },
+      { '<leader>E', '<cmd>Neotree focus<cr>', desc = 'Focus Explorer' },
+      { '<leader>fe', '<cmd>Neotree toggle<cr>', desc = 'Explorer (Root Dir)' },
+      { '<leader>fE', '<cmd>Neotree toggle dir=%:p:h<cr>', desc = 'Explorer (cwd)' },
+      { '<leader>ge', '<cmd>Neotree git_status<cr>', desc = 'Git Explorer' },
+      { '<leader>be', '<cmd>Neotree buffers<cr>', desc = 'Buffer Explorer' },
+    },
+    cmd = 'Neotree',
+    opts = {
+      sources = { 'filesystem', 'buffers', 'git_status', 'document_symbols' },
+      open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        mappings = {
+          ['<space>'] = 'none',
+          ['Y'] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg('+', path, 'c')
+            end,
+            desc = 'Copy Path to Clipboard',
+          },
+          ['O'] = {
+            function(state)
+              require('lazy.util').open(state.tree:get_node().path, { system = true })
+            end,
+            desc = 'Open with System Application',
+          },
+          ['P'] = { 'toggle_preview', config = { use_float = false } },
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = '',
+          expander_expanded = '',
+          expander_highlight = 'NeoTreeExpander',
+        },
+        git_status = {
+          symbols = {
+            unstaged = '󰄱',
+            staged = '󰱒',
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require('neo-tree').setup(opts)
+      vim.api.nvim_create_autocmd('TermClose', {
+        pattern = '*lazygit',
+        callback = function()
+          if package.loaded['neo-tree.sources.git_status'] then
+            require('neo-tree.sources.git_status').refresh()
+          end
+        end,
+      })
+    end,
   },
   {
     "antosha417/nvim-lsp-file-operations",
